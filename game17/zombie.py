79aa8559e8455f708bb43715489d207ca766a125
alan-reitsma <108203580+alan-reitsma@users.noreply.github.com>
2022-06-28 12:05:53 +1000

    Reinstated original zombie
    This module contains regular zombie and T800

--- game17/zombie.py
+++ game17/zombie.py
@@ -3,8 +3,43 @@ import numpy as np
 
 from .game17 import find_owned_pieces
 
-
 def make_moves(owner, rounds_left, owners, numbers):
+    """
+    Zombie mover. Moves each owned piece in a random direction.
+
+    Parameters
+    ----------
+    owner : int
+        Player number.
+    rounds_left : int
+        Maximum possible rounds left after this one.
+    owners : square array of ints
+        Current owner of each square.
+    numbers : square array of ints
+        Current number of pieces in each square.
+
+    Returns
+    -------
+    list of triples
+        Each tuple contains a 2-element numpy array of ints giving the
+        current coordinates of the pieces to be moved, a string giving the
+        direction to move ('n', 's', 'e', or 'w'), and an int giving the
+        number of pieces to move.
+
+    """
+    owned_pieces = find_owned_pieces(owner, owners, numbers)
+    moves = []
+    for pieces in owned_pieces:
+        coords = pieces[:2]
+        count = pieces[2]
+        to_move = np.random.multinomial(count, [0.25]*4)
+        for i in range(4):
+            if to_move[i] > 0:
+                move = (coords, 'nsew'[i], to_move[i])
+                moves.append(move)
+    return moves
+
+def make_moves_T800(owner, rounds_left, owners, numbers):
     """
     T800 mover. Initially moves each owned piece in a random direction. Once
     the android owns more than 2 squares it changes tactic to actively target
